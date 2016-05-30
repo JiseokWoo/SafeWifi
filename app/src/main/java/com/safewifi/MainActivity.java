62872390399b0f1607e65615d972af1088fc9415
Jiseok <davidwoo21@hanmail.net>
2016-05-30 23:13:11 +0900

    와이파이 스캔 코드 작성(...ing)
    아직 스캐닝 정상 작동 안됨..
    에뮬레이터라 그런가..?

--- app/src/main/java/com/safewifi/MainActivity.java
+++ app/src/main/java/com/safewifi/MainActivity.java
@@ -1,14 +1,21 @@
 package com.safewifi;
 
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private ListView lv_wifiList;
     private ArrayAdapter<String> adapter;
+    private WifiManager wifiManager;
+    private List<ScanResult> scanResultList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +29,26 @@ public class MainActivity extends AppCompatActivity {
         lv_wifiList = (ListView) findViewById(R.id.lv_wifiList);
         lv_wifiList.setAdapter(adapter);
 
-        // adapter에 item 추가
-        adapter.add("1");
-        adapter.add("2");
-        adapter.add("3");
-        adapter.add("4");
-        adapter.add("5");
-        adapter.add("6");
+        // wifi manager 생성
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        // wifi 비활성일 경우 활성화
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+        //while (!wifiManager.isWifiEnabled()) {}
+
+        // wifi list scan
+        if (wifiManager.isWifiEnabled() && wifiManager.startScan()) {
+            scanResultList = wifiManager.getScanResults();
+
+            // 스캔 결과 adapter에 추가
+            if (scanResultList != null && !scanResultList.isEmpty()) {
+                for (ScanResult wifi : scanResultList) {
+                    adapter.add(wifi.SSID);
+                }
+            }
+        }
 
         // 데이터 업데이트
         adapter.notifyDataSetChanged();
